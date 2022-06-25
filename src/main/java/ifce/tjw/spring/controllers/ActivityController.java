c95ce751f2a889d027fe76629a6b8209ff140509
Nityananda Barbosa <nityanandabarbosa@gmail.com>
2022-06-25 17:02:08 -0300

    feat : create endpoint to list all activities

--- src/main/java/ifce/tjw/spring/controllers/ActivityController.java
+++ src/main/java/ifce/tjw/spring/controllers/ActivityController.java
@@ -1,16 +1,12 @@
 package ifce.tjw.spring.controllers;
 
+import java.util.List;
 import java.util.Map;
 
 import ifce.tjw.spring.dto.ActivityCreateDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import ifce.tjw.spring.dto.ActivityCreatedDTO;
 import ifce.tjw.spring.services.ActivityService;
@@ -32,10 +28,27 @@ public class ActivityController {
         Map<String, String> payload = UserInfoToken.getUserInfoFromToken(token);
         Long userId = Long.parseLong(payload.get("id"));
         try {
-            ActivityCreatedDTO discipline = service.createActivity(dto, userId, id);
+            ActivityCreatedDTO createdDTO = service.createActivity(dto, userId, id);
 
-            if (discipline != null) {
-                return new ResponseEntity<>(discipline, HttpStatus.CREATED);
+            if (createdDTO != null) {
+                return new ResponseEntity<>(createdDTO, HttpStatus.CREATED);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<List<ActivityCreatedDTO>> createDiscipline(@PathVariable Long id,
+                                                                     @RequestHeader(name = "Authorization") String token) {
+        Map<String, String> payload = UserInfoToken.getUserInfoFromToken(token);
+        Long userId = Long.parseLong(payload.get("id"));
+        try {
+            List<ActivityCreatedDTO> dtoList = service.getAllActivities(userId, id);
+
+            if (dtoList != null) {
+                return new ResponseEntity<>(dtoList, HttpStatus.OK);
             }
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/ifce/tjw/spring/services/ActivityService.java
+++ src/main/java/ifce/tjw/spring/services/ActivityService.java
@@ -15,6 +15,9 @@ import ifce.tjw.spring.repositories.ActivityRepository;
 import ifce.tjw.spring.repositories.DisciplineRepository;
 import ifce.tjw.spring.repositories.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ActivityService {
     private final ActivityRepository repository;
@@ -48,6 +51,21 @@ public class ActivityService {
             repository.save(activity);
             disciplineRepository.save(discipline);
             return mapper.map(activity, ActivityCreatedDTO.class);
+        }
+        return null;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, readOnly = false)
+    public List<ActivityCreatedDTO> getAllActivities(Long userId, Long disciplineId) {
+        List<ActivityCreatedDTO> dtoList = new ArrayList<>();
+        Discipline discipline = disciplineRepository.getReferenceById(disciplineId);
+        User creator = userRepository.getReferenceById(userId);
+        if(discipline == null || creator == null){
+            return null;
+        }
+        if(discipline.getUser().contains(creator) || discipline.getOwner() == creator){
+            discipline.getActivities().forEach((activity -> dtoList.add(mapper.map(activity, ActivityCreatedDTO.class))));
+            return dtoList;
         }
         return null;
     }
